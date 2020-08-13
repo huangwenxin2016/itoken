2d4c0ae113f33007d9dac42d7f34bbc0ca0cc355
huangwenxin <1542171687@qq.com>
2020-08-13 14:59:51 +0800

    git commit 添加注解

--- itoken-admin/src/main/java/com/funtl/itoken/admin/AdminApplication.java
+++ itoken-admin/src/main/java/com/funtl/itoken/admin/AdminApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author huagnwenxin
+ * @date 2020-08-13
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableAdminServer
